135c58c0a21948c740b74a3b8c803fb3ca3c0c53
koloz <koloz@DESKTOP-DG1A95Q>
2025-12-15 18:02:37 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kolozsvari_Balint_Istvan_Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
